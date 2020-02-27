6dfa1b7680391af39c92775a08f07f2f2f3ec0ca
tdryoji <ri76joy@gmail.com>
2020-02-28 07:42:38 +0900

    3次元ベクトルのクラスの雛形を作成

---
+++ 3DVector/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3DVector
+{
+    class Vector
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+
+        public Vector(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        public Vector() : this(0, 0, 0) { }
+
+        public override string ToString()
+        {
+            return "(" + this.X.ToString()
+                + ", " + this.Y.ToString()
+                + ", " + this.Z.ToString() + ")";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector();
+            Console.WriteLine("v1 = " + v1.ToString());
+        }
+    }
+}
